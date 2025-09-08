10649d40f84505d8508db174843941aa4d67d471
yoonsooho <7552678@naver.com>
2025-09-09 07:12:17 +0900

    fix : next.config 설정 수정

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
     async rewrites() {
         return [
             {
+                source: "/api/auth/refresh-token",
+                destination: "/api/auth/refresh-token", // 내부 API 라우트 유지
+            },
+            {
                 source: "/api/:path*",
                 destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
             },
