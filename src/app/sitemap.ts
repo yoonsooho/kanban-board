665ba7f028f29d4d9a4407a2effefa1e2dd340e5
yoonsooho <7552678@naver.com>
2025-10-02 13:42:32 +0900

    fix : sitmap.ts파일 중복 코드 수정

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,19 +2,20 @@ import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
     const baseUrl = "https://goaldiary.vercel.app";
+    const now = new Date();
 
     return [
         {
-            url: "https://goaldiary.vercel.app",
-            lastModified: new Date(),
-            changeFrequency: "daily", // 매일 업데이트됨을 알림
-            priority: 1, // 최고 우선순위
+            url: baseUrl,
+            lastModified: now,
+            changeFrequency: "daily",
+            priority: 1,
         },
         {
-            url: "https://goaldiary.vercel.app/main",
-            lastModified: new Date(),
+            url: `${baseUrl}/main`,
+            lastModified: now,
             changeFrequency: "daily",
-            priority: 0.8, // 높은 우선순위
+            priority: 0.8,
         },
     ];
 }
