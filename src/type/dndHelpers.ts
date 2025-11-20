9d640907783c0670206402f9eced136894b47f33
yoonsooho <7552678@naver.com>
2025-11-21 00:27:17 +0900

    fix : DndHelpers 타입 에러 수정

--- src/type/dndHelpers.ts
+++ src/type/dndHelpers.ts
@@ -4,5 +4,7 @@ export interface DndHelpers {
     isSomeBoard: (id: number) => boolean;
     findBoardIdx: (itemId: number) => number;
     findBoard: (itemId: number) => board | undefined;
-    getBoardItems: (boardIndex: number) => { id: number; text: string }[];
+    getBoardItems: (
+        boardIndex: number
+    ) => { id: number; text: string; startTime: string | null; endTime: string | null }[];
 }
